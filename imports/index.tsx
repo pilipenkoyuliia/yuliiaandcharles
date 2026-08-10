9ede68469407db4ceec06d12a063bd5e7c38b519
pilipenkoyuliia <pilipenko2020@gmail.com>
2026-08-10 19:10:51 +0200

    Add files via upload

---
+++ imports/index.tsx
@@ -0,0 +1,44 @@
+import svgPaths from "./svg-iugvc5ugo9";
+import img1F7Eac7D11378B8845Af3160822335Ac1 from "./0d003d276b5f699b089fdaba06551f820679ce6e.png";
+import img909B1A004C31A9C26B538D13Bcec37B11 from "./8c531ef921461af3276d3e75412321ce27b5217c.png";
+
+export default function Hero() {
+  return (
+    <div className="bg-[#f3f1ed] relative size-full" data-name="Hero1">
+      <div className="absolute h-[526px] left-[326px] top-[359px] w-[788px]" data-name="1f7eac7d11378b8845af3160822335ac 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={img1F7Eac7D11378B8845Af3160822335Ac1} />
+      </div>
+      <div className="absolute h-[71px] left-[691px] top-[460px] w-[58px]" data-name="Vector">
+        <svg className="absolute block inset-0 size-full" fill="none" height="71" preserveAspectRatio="none" viewBox="0 0 58 71" width="58">
+          <g id="Vector">
+            <path d={svgPaths.p3d5709f0} fill="black" />
+            <path d={svgPaths.p3da82000} fill="black" />
+            <path d={svgPaths.pf36d00} fill="black" />
+            <path d={svgPaths.p3dd8e380} fill="black" />
+            <path d={svgPaths.p2848f480} fill="black" />
+            <path d={svgPaths.p18741580} fill="black" />
+            <path d={svgPaths.p34cc6980} fill="black" />
+            <path d={svgPaths.p2ebe6c80} fill="black" />
+            <path d={svgPaths.p26e5bf00} fill="black" />
+            <path d={svgPaths.p283b140} fill="black" />
+            <path clipRule="evenodd" d={svgPaths.pbba480} fill="black" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p2a89ed80} fill="black" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p13426e80} fill="black" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p1645ecf0} fill="black" fillRule="evenodd" />
+            <path d={svgPaths.p201d3400} fill="black" />
+          </g>
+        </svg>
+      </div>
+      <p className="[word-break:break-word] absolute font-['Avenir:Heavy',sans-serif] leading-[normal] left-[606px] not-italic text-[#2c333c] text-[16px] top-[185px] tracking-[0.96px] whitespace-nowrap">YOU HAVE AN INVITATION</p>
+      <p className="[word-break:break-word] absolute font-['Cormorant_Garamond:Regular',sans-serif] font-normal leading-[normal] left-[581px] text-[49px] text-black top-[271px] tracking-[-2.94px] whitespace-pre">{`Yuliia  & Charles`}</p>
+      <div className="absolute flex h-[229.804px] items-center justify-center left-[609px] top-[489px] w-[221.406px]">
+        <div className="flex-none rotate-[-142.76deg]">
+          <div className="h-[183px] relative w-[139px]" data-name="909b1a004c31a9c26b538d13bcec37b1 1">
+            <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={img909B1A004C31A9C26B538D13Bcec37B11} />
+          </div>
+        </div>
+      </div>
+      <p className="[word-break:break-word] absolute font-['Ephesis:Regular',sans-serif] leading-[normal] left-[699px] not-italic text-[30px] text-black top-[229px] tracking-[0.3px] whitespace-nowrap">from</p>
+    </div>
+  );
+}
